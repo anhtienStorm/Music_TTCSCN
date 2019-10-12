2e16f7445690532b38be75067e76906df06138ce
tiennab <tiennab@bkav.com>
2019-10-12 16:27:22 +0700

    12/10 16:27

--- app/src/main/java/com/example/activitymusic/Adapter/BannerAdapter.java
+++ app/src/main/java/com/example/activitymusic/Adapter/BannerAdapter.java
@@ -1,13 +1,18 @@
 package com.example.activitymusic.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
+import com.bumptech.glide.Glide;
 import com.example.activitymusic.Model.SongOnline;
+import com.example.activitymusic.R;
 
 import java.util.ArrayList;
 
@@ -15,6 +20,11 @@ public class BannerAdapter extends PagerAdapter {
 
     Context mContext;
     ArrayList<SongOnline> mSongOnlineList = new ArrayList<>();
+
+    public BannerAdapter(Context context, ArrayList<SongOnline> list){
+        mContext = context;
+        mSongOnlineList = list;
+    }
 
     @Override
     public int getCount() {
@@ -29,6 +39,25 @@ public class BannerAdapter extends PagerAdapter {
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        return super.instantiateItem(container, position);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.row_baner,null);
+
+        ImageView imgBackgroundBanner = view.findViewById(R.id.img_background_banner);
+        ImageView imgIconBanner = view.findViewById(R.id.img_icon_banner);
+        TextView textViewTitleBanner = view.findViewById(R.id.tv_title_banner);
+        TextView textViewContentBanner = view.findViewById(R.id.tv_content_banner);
+
+        Glide.with(mContext).load(mSongOnlineList.get(position).getIMAGE()).into(imgBackgroundBanner);
+        Glide.with(mContext).load(mSongOnlineList.get(position).getIMAGE()).into(imgIconBanner);
+        textViewTitleBanner.setText(mSongOnlineList.get(position).getNAMESONG());
+        textViewContentBanner.setText(mSongOnlineList.get(position).getSINGER());
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
     }
 }
--- app/src/main/java/com/example/activitymusic/Fragment/BannerFragment.java
+++ app/src/main/java/com/example/activitymusic/Fragment/BannerFragment.java
@@ -1,7 +1,7 @@
 package com.example.activitymusic.Fragment;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +9,9 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.activitymusic.Adapter.BannerAdapter;
 import com.example.activitymusic.Model.SongOnline;
 import com.example.activitymusic.R;
 import com.example.activitymusic.Server.APIServer;
@@ -18,17 +20,33 @@ import com.example.activitymusic.Server.DataServer;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.relex.circleindicator.CircleIndicator;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class BannerFragment extends Fragment {
+
+    View mView;
+    ViewPager mViewPager;
+    CircleIndicator mCircleIndicator;
+    BannerAdapter mBannerAdapter;
+    Runnable mRunnable;
+    Handler mHandler;
+    int mCurrentItem;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.banner_fragment, container, false);
+        mView = inflater.inflate(R.layout.banner_fragment, container, false);
+        initview();
         getData();
-        return view;
+        return mView;
+    }
+
+    void initview(){
+        mViewPager = mView.findViewById(R.id.viewpager_banner);
+        mCircleIndicator = mView.findViewById(R.id.indicator_banner);
     }
 
     private void getData(){
@@ -37,7 +55,24 @@ public class BannerFragment extends Fragment {
         callback.enqueue(new Callback<List<SongOnline>>() {
             @Override
             public void onResponse(Call<List<SongOnline>> call, Response<List<SongOnline>> response) {
-                ArrayList<SongOnline> listSongOnline = (ArrayList<SongOnline>) response.body();
+                ArrayList<SongOnline> songOnlineList = (ArrayList<SongOnline>) response.body();
+                mBannerAdapter = new BannerAdapter(getActivity(),songOnlineList);
+                mViewPager.setAdapter(mBannerAdapter);
+                mCircleIndicator.setViewPager(mViewPager);
+                mHandler = new Handler();
+                mRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        mCurrentItem = mViewPager.getCurrentItem();
+                        mCurrentItem++;
+                        if (mCurrentItem >= mViewPager.getAdapter().getCount()){
+                            mCurrentItem = 0;
+                        }
+                        mViewPager.setCurrentItem(mCurrentItem,true);
+                        mHandler.postDelayed(mRunnable,5000);
+                    }
+                };
+                mHandler.postDelayed(mRunnable,5000);
             }
 
             @Override
